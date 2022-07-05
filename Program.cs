672aea0d2b47ff37f3ba2195923cf99108015b3c
Tjwoody <105826710+Tjwoody@users.noreply.github.com>
2022-07-05 18:35:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão_5_Djalma_numeros_de_1_100
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int numeros;
+            for (numeros = 1; numeros < 100; numeros++)
+                {
+                    numeros = numeros+1 ;
+                    Console.WriteLine(numeros);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
